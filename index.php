f97f100d6d9c6131a8d9ee4f802ecf26daaf468d
Marco Giammari <marcogiammari@gmail.com>
2023-06-09 15:27:11 +0200

    add include (functions.php)

--- index.php
+++ index.php
@@ -11,16 +11,7 @@
 
 <?php 
 
-function generatePassword($length) {
-    $characters = 'qwertyuiopasdfghjklzxcvbnm1234567890';
-    $charactersSplit = str_split($characters);
-    $password = "";
-    for ($i=0; $i < $length; $i++) { 
-        
-        $password .= $characters[rand(0, count($charactersSplit))];
-    }
-    echo $password;
-}
+include_once './functions.php'
 
 ?>
 
